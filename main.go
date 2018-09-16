2861398ed42ff0df7cd3d986343f924afc98c903
Larry W. Cashdollar <larry0@me.com>
2018-09-16 10:00:51 -0400

    formatting

--- main.go
+++ main.go
@@ -1,38 +1,40 @@
 package main
+//TODO 
+// make a configuration file that we can change http port and IP
+// select different home routers to emulate
+// toggle console logging
 
 import (
-  "net/http"
-  "log"
-  "os"
-  "fmt"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
-
 func main() {
-        console_log := 1
-        port := ":8080"
+	console_log := 1
+	port := ":8080"
 	logPath := "access.log"
 	writeLogFile(logPath)
-  fmt.Println("WAPot v1.0\n\n[+] Listening on port",port) 
-  http.Handle("/", http.FileServer(http.Dir("./http")))
-  if err := http.ListenAndServe(port, logRequest(http.DefaultServeMux,console_log)) 
-  err != nil {
-    panic(err)
-  }
+	fmt.Println("WAPot v1.0\n\n[+] Listening on port", port)
+	http.Handle("/", http.FileServer(http.Dir("./http")))
+	if err := http.ListenAndServe(port, logRequest(http.DefaultServeMux, console_log)); err != nil {
+		panic(err)
+	}
 
-  }
+}
 
 func logRequest(handler http.Handler, clog int) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		log.Printf("%s %s %s \"%s\" \"%s\" \n", req.RemoteAddr, req.Header.Get("X-Forwarded-For"),req.Method, req.URL.RequestURI(), req.UserAgent())
-	        fmt.Printf("%s %s %s \"%s\" \"%s\" \n", req.RemoteAddr, req.Header.Get("X-Forwarded-For"),req.Method, req.URL.RequestURI(), req.UserAgent())
+		log.Printf("%s %s %s \"%s\" \"%s\" \n", req.RemoteAddr, req.Header.Get("X-Forwarded-For"), req.Method, req.URL.RequestURI(), req.UserAgent())
+		fmt.Printf("%s %s %s \"%s\" \"%s\" \n", req.RemoteAddr, req.Header.Get("X-Forwarded-For"), req.Method, req.URL.RequestURI(), req.UserAgent())
 		handler.ServeHTTP(w, req)
 	})
 }
 
 func writeLogFile(logfile string) {
-if logfile != "" {
-	line, err := os.OpenFile(logfile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0640)
+	if logfile != "" {
+		line, err := os.OpenFile(logfile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0640)
 		if err != nil {
 			log.Fatal("OpenLogfile: os.OpenFile:", err)
 		}
@@ -40,3 +42,4 @@ if logfile != "" {
 		log.SetOutput(line)
 	}
 }
+
